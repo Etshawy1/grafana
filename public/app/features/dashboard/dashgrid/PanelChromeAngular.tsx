f29fb15429e7844c45739b50ef40ac2fe9e74b71
Muhammad Hesham <muhesham@microsoft.com>
2024-03-24 14:08:08 +0200

    refresh panel 10.4.1

--- public/app/features/dashboard/dashgrid/PanelChromeAngular.tsx
+++ public/app/features/dashboard/dashgrid/PanelChromeAngular.tsx
@@ -210,6 +210,7 @@ export class PanelChromeAngularUnconnected extends PureComponent<Props, State> {
         displayMode={transparent ? 'transparent' : 'default'}
         onCancelQuery={panelChromeProps.onCancelQuery}
         onOpenMenu={panelChromeProps.onOpenMenu}
+        panel={panel}
       >
         {() => <div ref={(element) => (this.element = element)} className="panel-height-helper" />}
       </PanelChrome>
